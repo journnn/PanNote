86c07ea8cbf4fd29877bb06c1be728560f3f9d30
journnn <journeyreyes14@gmail.com>
2024-08-12 16:38:41 +0800

    Update signup.php

--- pages/signup.php
+++ pages/signup.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="styles.css" type="text/css" media="all" />
+    <link rel="stylesheet" href="../styles/signup.css" type="text/css" media="all" />
     <title>PanNote</title>
 </head>
 <body>
@@ -18,5 +18,6 @@
         <br>
         <button id="signupBtn">Sign up</button>
     </div>
+     <a href="login.php">Log in</a>
 </body>
 </html>
