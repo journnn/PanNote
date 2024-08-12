ba235e47efadf1c652b995e68cb0b69f06a3f795
journnn <journeyreyes14@gmail.com>
2024-08-12 14:39:14 +0800

    login

---
+++ pages/login.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css" type="text/css" media="all" />
+    <title>PanNote</title>
+</head>
+<body>
+    <div class="form-container">
+        <img src="../assets/logo.png" alt="Pa-Note Logo">
+        <form id="loginForm" action="login.php" method="POST">
+            <input type="text" name="username" placeholder="Username" required>
+            <input type="password" name="password" placeholder="Password" required>
+            <button type="submit">Log in</button>
+        </form>
+        <br>
+        <button id="signupBtn">Sign up</button>
+    </div>
+</body>
+</html>
